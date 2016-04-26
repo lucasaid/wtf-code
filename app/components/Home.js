3d508b0c629007a952d2d48900c8ddf706ed57f9
Chris Lucas <chrislucas@192-168-1-4.tpgi.com.au>
2016-04-26 22:11:13 +1000

    feat(Added components and route): Added app.js and main and home components, also set up routes for

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Home = React.createClass({
+  render: function(){
+    return (
+      <h2 className="text-center">
+        Search By WTF you want to know.
+      </h2>
+    )
+  }
+});
+
+module.exports = Home;
