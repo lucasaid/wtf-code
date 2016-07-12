5f5af5a14ccfe52386fd5122fb056db5b2593383
Chris Lucas <chrislucas@Chriss-MacBook-Pro.local>
2016-07-12 15:56:56 +1000

    Added webpack, changed layout

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,9 +3,13 @@ var React = require('react');
 var Home = React.createClass({
   render: function(){
     return (
-      <h2 className="text-center">
-        Search By WTF you want to know.
-      </h2>
+      <div className="col-md-12">
+        <h2 className="text-center">
+          Search By WTF you want to know.
+        </h2>
+        <label className="col-md-4">WTF is...</label>
+        <input type="text" className="form-control" />
+      </div>
     )
   }
 });
